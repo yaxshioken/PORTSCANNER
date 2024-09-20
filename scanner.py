8098aa0fe282ed0b989663d378bac51119f7e241
yaxshioken <yaxshioken@gmail.com>
2024-09-21 00:01:49 +0500

    Colorama Qo'shildi

--- scanner.py
+++ scanner.py
@@ -1,17 +1,21 @@
 #!/usr/bin/python
 import socket  # socket kutubxonasini import qilib olamiz
-
+from termcolor import colored
 conn = socket.socket(socket.AF_INET, socket.SOCK_STREAM)  # va bular orqali connection hosil qilamiz
+
+socket.setdefaulttimeout(5)
 # IPv4 va IPv6 bog'lanishlarni uchun ""socket.AF_INET,socket.SOCK_STREAM""
-host = "127.0.0.1"  # bu bizning local hostimiz
-port = 80  # bu port raqami
+host = input("-[#]- IP Manzilni kiriting:")  # bu bizning local hostimiz
+# port = int(input("-[#]- Port raqamini kiriting:"))   # bu port raqami
 
 
 def portscanner(port):  # method yaratamiz
     if conn.connect_ex((host, port)):  # bu yerda portga  connection hosil qila olmasa xabar beradi
-        print("%d porti yopiq ekan" % (port))
+        print(colored("%d porti yopiq ekan" % (port),'grey',attrs=['reverse']))
     else:
-        print("%d porti yopiq ekan" % (port))
+        print(colored("%d _________[[[[ porti ochiq ekan ]]]]______________" % (port),'green',attrs=['bold']))
 
 
-portscanner(port)  # methodimizni ishlatamiz
+for port in range(1, 10000):
+    portscanner(port)
+
